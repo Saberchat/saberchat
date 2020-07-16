532aa69b49c6f8bfeff3a350c4ad53b1fc2c3c9d
vdarsha <66336210+vdarsha@users.noreply.github.com>
2020-07-16 14:09:28 -0700

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,12 @@
+var input = document.getElementById("myInput");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
