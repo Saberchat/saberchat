3e0780b0427ac0fceb138360d1106438502c3e09
Alexander Zhou <alexzhou3999@gmail.com>
2020-07-28 15:23:27 -0700

    Reworked code and cleaned it up. Also added some special effects

---
+++ public/app.js
@@ -0,0 +1,8 @@
+$(function() {
+    $(".user-form-btn").on('click', function(){
+        $(".user-login-form").fadeToggle(500);
+    });
+    $(".form-close").on('click', function(){
+        $(".user-login-form").fadeToggle(500);
+    });
+});
