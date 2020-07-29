f4a14e69c5105e14bcdfc92361bf93653b8241c2
Alexander Zhou <alexzhou3999@gmail.com>
2020-07-29 15:14:00 -0700

    Redid the signup/login form animations. Added in placeholder links. reworked htmlclasses and css a little
    Made some various improvements. It's all in the title

--- public/app.js
+++ public/app.js
@@ -1,8 +1,23 @@
 $(function() {
-    $(".user-form-btn").on('click', function(){
-        $(".user-login-form").fadeToggle(500);
+    $(".user-form-btn.login").on('click', function(){
+        // $(".user-form.login").fadeIn(500);
+        // $(".user-form.signup").fadeOut(500);
+        $(".user-form.login").addClass("display");
+        $(".user-form.signup").removeClass("display");
     });
-    $(".form-close").on('click', function(){
-        $(".user-login-form").fadeToggle(500);
+    $(".form-close.login").on('click', function(){
+        // $(".user-form.login").fadeOut(500);
+        $(".user-form.login").removeClass("display");
+    });
+
+    $(".user-form-btn.signup").on('click', function(){
+        // $(".user-form.signup").fadeIn(500);
+        // $(".user-form.login").fadeOut(500);
+        $(".user-form.signup").addClass("display");
+        $(".user-form.login").removeClass("display");
+    });
+    $(".form-close.signup").on('click', function(){
+        // $(".user-form.signup").fadeOut(500);
+        $(".user-form.signup").removeClass("display");
     });
 });
