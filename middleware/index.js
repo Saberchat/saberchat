a0fd8ad6047d2432d1549f23a34100a8fe2883bb
schavan <siddharth@chavans.net>
2020-08-08 15:34:33 -0700

    comment

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,9 @@
+// Executes before the code in HTTP route request
 middlewareObj = {};
 
 middlewareObj.isLoggedIn = function(req, res, next) {
     if(req.isAuthenticated()) {
-        return next();
+      return next();
     }
     req.flash('error', 'Please Login');
     res.redirect('/');
