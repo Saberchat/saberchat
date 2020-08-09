e86a03011b77a8a0b53440670b867dd881f845ba
Alexander Zhou <alexzhou3999@gmail.com>
2020-08-09 10:27:01 -0700

    Made user profile page dynamic

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,10 @@
 const   express = require('express'),
         router = express.Router();
+        
+const middleware = require('../middleware');
 
 //renders views/profile/index.ejs at /profile route.
-router.get('/profile', function(req, res) {
+router.get('/profile', middleware.isLoggedIn, function(req, res) {
     res.render('profile/index');
 });
 
