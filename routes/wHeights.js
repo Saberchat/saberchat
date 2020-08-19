b2be70bdc08d47e28f419abe93d6b3437c64dd18
Alexander Zhou <alexzhou3999@gmail.com>
2020-08-19 16:26:10 -0700

    started working on the article editor

--- routes/wHeights.js
+++ routes/wHeights.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
     res.render('wHeights/index', {articles: [0,1,2,3,4,5,6,7,8,9]});
 });
 
+router.get('/articles/new', function(req, res) {
+    res.render('wHeights/new');
+});
+
 module.exports = router;
