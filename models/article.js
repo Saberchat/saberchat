73a40e6df0c86effcd9bd0b47576e734698710c1
Alexander Zhou <alexzhou3999@gmail.com>
2020-08-20 15:04:51 -0700

    Got the article creation ui working!

--- models/article.js
+++ models/article.js
@@ -7,10 +7,7 @@ var articleSchema = new mongoose.Schema({
     content: [
         {type: Object}
     ],
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
+    author: String
 }, {timestamps: {createdAt: 'created_at'}});
 
 module.exports = mongoose.model("Article", articleSchema);
