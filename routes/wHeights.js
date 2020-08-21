9a94a70c66d9982d675f710142e834646c3fdab2
Alexander Zhou <alexzhou3999@gmail.com>
2020-08-21 11:01:24 -0700

    added articles

--- routes/wHeights.js
+++ routes/wHeights.js
@@ -2,21 +2,52 @@ const express = require('express');
 const router = express.Router();
 const middleware = require('../middleware');
 
+const Article = require('../models/article');
+
+// index page
 router.get('/', function(req, res) {
-    res.render('wHeights/index', {articles: [0,1,2,3,4,5,6,7,8,9]});
+    Article.find({}, function(err, foundArticles) {
+        if(err) {
+            req.flash('error', 'Cannot access Database');
+            res.redirect('/witherlyheights');
+        } else {
+            res.render('wHeights/index', {articles: foundArticles});
+        }
+    });
 });
 
+// display specific article
+router.get('/articles/:id', function(req, res) {
+    Article.findById(req.params.id, function(err, foundArticle) {
+        if(err) {
+            req.flash('error', 'Cannot find article');
+            res.redirect('/witherlyheights');
+        } else {
+            res.render('wHeights/show', {article: foundArticle});
+        }
+    });
+});
+
+// display form for creating articles
 router.get('/articles/new', function(req, res) {
     res.render('wHeights/new');
 });
 
+// create articles
 router.post('/articles/new', function(req, res) {
-    if(req.body) {
-        console.log('received new!');
-        console.log(req.body);
-        console.log(JSON.parse(req.body.content));
-    }
-    res.redirect('/witherlyheights');
+    const content = JSON.parse(req.body.content);
+    const articleObj = {
+        title: req.body.title,
+        author: req.body.author,
+        content: content
+    };
+    Article.create(articleObj, function(err, article) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/witherlyheights');
+        }
+    });
 });
 
 module.exports = router;
