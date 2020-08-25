c413c2fa840ea70b4f1a6207ded0a60067579863
Alexander Zhou <alexzhou3999@gmail.com>
2020-08-25 11:16:49 -0700

    small tweaks

--- public/javascript/admin/mod.js
+++ public/javascript/admin/mod.js
@@ -1,9 +1,11 @@
 function ignoreComment(button) {
+    const listItem = (button.parentNode).parentNode;
     const url = '/admin/moderate?_method=put';
     const commentId = button.id;
     $.post(url, {id: commentId}, function(data) {
         if(data.success) {
             console.log(data.success);
+            listItem.remove();
         } else if(data.error) {
             console.log(data.error);
         }
@@ -11,11 +13,13 @@ function ignoreComment(button) {
 }
 
 function deleteComment(button) {
+    const listItem = (button.parentNode).parentNode;
     const url = '/admin/moderate?_method=delete';
     const commentId = button.id;
     $.post(url, {id: commentId}, function(data) {
         if(data.success) {
             console.log(data.success);
+            listItem.remove();
         } else if(data.error) {
             console.log(data.error);
         }
