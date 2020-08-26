9c90bb09d50b19bfb69a95753bd4e0e8720b9c9d
KabirSamsi <samsikabir@gmail.com>
2020-08-25 17:16:22 -0700

    Updated inbox filesystem and route structure

--- routes/inbox.js
+++ routes/inbox.js
@@ -1,12 +1,68 @@
 const express = require('express');
+const middleware = require('../middleware');
 //start express router
 const router = express.Router();
 
-const Notif = require('../models/notification');
 
-//Function to display user inbox
-router.get('/inbox', (req, res) => {
-	res.render('inbox/index');
-});
+const User = require('../models/user');
+const Notification = require('../models/notification');
+
+//Access sendNotification file
+router.get('/notif', middleware.isLoggedIn, (req, res, next) => {
+	let types = ["Cafe Order Status Update", "Field Trip Notification", "PE Notification", "School Event Notification", "Class Schedule Change"]
+	User.find({}, (err, foundUsers) => {
+		if(err || !foundUsers) {
+			req.flash('error', 'Unable to access Database');
+			res.redirect('back');
+		} else {
+			res.render('inbox/sendNotification', {users: foundUsers, types})
+		}
+	})
+})
+
+//Route to send 'notification', different from 'comment'
+router.post('/new', middleware.isLoggedIn, (req, res) => {
+	let recipient = req.body.recipient
+	User.find({}, (err, foundUsers) => {
+		if(err || !foundUsers) {
+			req.flash('error', 'Unable to access Database');
+			res.redirect('back');
+		} else {
+			for (let i of foundUsers) {
+				if (i.username.toLowerCase() == recipient.toLowerCase()) {
+					Notification.create({type: req.body.type, sender: req.user, text: req.body.message}, (err, notification) => {
+						notification.save()
+						i.inbox.push(notification)
+						i.save()
+						req.flash('success', "Notification sent!")
+						res.redirect('/notif')
+					})
+				}
+			}
+		}
+	})
+})
+
+//Route to display user inbox
+router.get('/inbox', middleware.isLoggedIn, (req, res, next) => {
+	let notifList = []
+	Notification.find({
+
+	}).populate({path: 'sender', select: ['username']})
+	.exec((err, foundNotifs) => {
+		if (err || !foundNotifs) {
+			req.flash('error', 'Unable to access Database');
+				res.redirect('back')
+
+		} else {
+			for (let notif of foundNotifs) {
+				if (req.user.inbox.indexOf(notif['_id']) > -1) {
+					notifList.push(notif)
+				}
+			}
+			res.render('inbox/inbox', {username: req.user.username, notifs: notifList})
+		}
+	})
+})
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -81,64 +81,6 @@ router.post('/login', function(req, res, next) {
     })(req, res, next);
 });
 
-//Access sendNotification file
-router.get('/sendNotification', middleware.isLoggedIn, (req, res, next) => {
-	let types = ["Cafe Order Status Update", "Field Trip Notification", "PE Notification", "School Event Notification", "Class Schedule Change"]
-	User.find({}, (err, foundUsers) => {
-		if(err || !foundUsers) {
-			req.flash('error', 'Unable to access Database');
-			res.redirect('back');
-		} else {
-			res.render('chat/sendNotification', {users: foundUsers, types})
-		}
-	})
-})
-
-//Route to send 'notification', different from 'comment'
-router.post('/sendNotif', middleware.isLoggedIn, (req, res) => {
-	let recipient = req.body.recipient
-	User.find({}, (err, foundUsers) => {
-		if(err || !foundUsers) {
-			req.flash('error', 'Unable to access Database');
-			res.redirect('back');
-		} else {
-			for (let i of foundUsers) {
-				if (i.username.toLowerCase() == recipient.toLowerCase()) {
-					Notification.create({type: req.body.type, sender: req.user, text: req.body.message}, (err, notification) => {
-						notification.save()
-						i.inbox.push(notification)
-						i.save()
-						req.flash('success', "Notification sent!")
-						res.redirect('/sendNotification')
-					})
-				}
-			}
-		}
-	})
-})
-
-//Route to display user inbox
-router.get('/inbox', middleware.isLoggedIn, (req, res, next) => {
-	let notifList = []
-	Notification.find({
-
-	}).populate({path: 'sender', select: ['username']})
-	.exec((err, foundNotifs) => {
-		if (err || !foundNotifs) {
-			req.flash('error', 'Unable to access Database');
-				res.redirect('back')
-
-		} else {
-			for (let notif of foundNotifs) {
-				if (req.user.inbox.indexOf(notif['_id']) > -1) {
-					notifList.push(notif)
-				}
-			}
-			res.render('chat/inbox', {username: req.user.username, notifs: notifList})
-		}
-	})
-})
-
 // >>>>>>> c413c2fa840ea70b4f1a6207ded0a60067579863
 //logout route
 router.get("/logout", function(req, res) {
