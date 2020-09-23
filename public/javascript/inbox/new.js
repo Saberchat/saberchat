73b0f84d398ea0a302d458bc81cd11a88773ebd2
Alexander Zhou <alexzhou3999@gmail.com>
2020-09-22 17:21:11 -0700

    more work

--- public/javascript/inbox/new.js
+++ public/javascript/inbox/new.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('message-form');
 
 const userSelect = document.getElementById('user-select');
+const userDisplay = document.getElementById('user-display');
+
+let recipients = [];
 
 // processes all the selected recipients into form info
 function process() {
@@ -17,8 +20,25 @@ function updateTo(check) {
     if(check.checked) {
         userSelect.required = false;
         userSelect.style.display = 'none';
+        userDisplay.style.display = 'none';
     } else {
         userSelect.required = true;
         userSelect.style.display = '';
+        userDisplay.style.display = '';
     }
+}
+
+// adds recipients to list
+function addRecipient() {
+    const info = userSelect.value;
+    console.log(JSON.parse(users));
+    recipients.push(userSelect.value);
+    const tag = document.createElement('div');
+
+    console.log(recipients);
+}
+
+// refreshes the user list display
+function refreshUserList() {
+
 }
