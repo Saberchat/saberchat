d5cae2c48e1a304fe9ed7a60a2ad0671222da079
KabirSamsi <samsikabir@gmail.com>
2020-10-25 14:15:37 -0700

    Added some js which shows images when you put a link

--- public/javascript/inbox/form.js
+++ public/javascript/inbox/form.js
@@ -5,12 +5,28 @@ let i = 0;
 function addImg() {
     const img = document.createElement('div');
     img.classList.add('image-group');
+    img.id = `block-${i}`
     img.innerHTML = `<div class="input-container">
-    <input type="text" class="form-control" placeholder="Url..." name="images[${i}]" required>
+    <input type="text" class="form-control" onkeyup="createImg(this)" placeholder="Url..." name="images[${i}]" required>
   </div>
   <button type="button" onclick="deleteImg(this)" class="btn btn-danger"><i class="fas fa-minus"></i></button>`;
     imgContainer.appendChild(img);
     i++;
+}
+
+function createImg(val) { //Creates an image based on value of image input
+
+  if ($(`#block-${i-1}`).find('img').length == 0) {
+    const imageDisplay = document.createElement('img')
+    imageDisplay.src = val.value
+    imageDisplay.style = "width: 50%; height: 50%; margin-top: 10px; border-radius: 15px;"
+    document.getElementById(`block-${i-1}`).appendChild(imageDisplay)
+
+  } else {
+    setTimeout(() => {
+      $(`#block-${i-1}`).find('img')[0].src = val.value
+    }, 3)
+  }
 }
 
 function deleteImg(btn) {
