9c1020c2e1255b72fb1d75a5af84355c9d18c7e7
KabirSamsi <samsikabir@gmail.com>
2020-10-26 10:29:07 -0700

    Set up editing profile/email/pwd

--- routes/profile.js
+++ routes/profile.js
@@ -47,33 +47,53 @@ router.get('/:id', middleware.isLoggedIn, function(req, res) {
 
 // update user route. Check if current user matches profiles they're trying to edit with middleware.
 router.put('/profile', middleware.isLoggedIn, function(req, res) {
-  let user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: filter.clean(req.body.username),
-    description: filter.clean(req.body.description),
-    title: filter.clean(req.body.title),
-    status: req.body.status
-  }
 
-  if(req.body.imageUrl) {
-      user.imageUrl = req.body.imageUrl;
-  }
-  if(req.body.bannerUrl) {
-      user.bannerUrl = req.body.bannerUrl;
-  }
+  (async() => {
 
+    const overlap = await User.find({username: filter.clean(req.body.username), _id: {$nin: [req.user._id]}});
 
-  //find and update the user with new info
-  User.findByIdAndUpdate(req.user._id, user, function(err, updatedUser) {
-    if(err || !updatedUser) {
-        req.flash('error', 'There was an error updating your profile');
-        res.redirect('back');
-    } else {
-        req.flash('success', 'Updated your profile');
-        res.redirect('/profiles/' + req.user._id);
+    if (!overlap) {
+      req.flash('error', "Unable to find users");
+      return res.redirect('back');
+
+    } else if (overlap.length > 0) {
+      req.flash('error', "Another user already has that username.");
+      return res.redirect('back');
     }
-  });
+
+    let user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: filter.clean(req.body.username),
+      description: filter.clean(req.body.description),
+      title: filter.clean(req.body.title),
+      status: req.body.status
+    }
+
+    if(req.body.imageUrl) {
+        user.imageUrl = req.body.imageUrl;
+    }
+    if(req.body.bannerUrl) {
+        user.bannerUrl = req.body.bannerUrl;
+    }
+
+
+    //find and update the user with new info
+    const updatedUser = await User.findByIdAndUpdate(req.user._id, user);
+
+    if(!updatedUser) {
+      req.flash('error', 'There was an error updating your profile');
+      return res.redirect('back');
+    }
+
+    req.flash('success', 'Updated your profile');
+    res.redirect('/profiles/' + req.user._id);
+
+  })().catch(err => {
+    console.log(err);
+    req.flash('error', "Unable to access database");
+    res.redirect('back');
+  })
 });
 
 //route for changing email. Similar to edit profiles route. But changing email logs out user for some reason.
@@ -87,7 +107,18 @@ router.put('/change-email', middleware.isLoggedIn, function(req, res) {
       return res.redirect('back');
 
     } else if (emails.length < 1) {
-      req.flash('error', "Email not in whitelist");
+      req.flash('error', "That email is not in our whitelist");
+      return res.redirect('back');
+    }
+
+    const overlap = await User.find({email: req.body.email, _id: {$nin: [req.user._id]}});
+
+    if (!overlap) {
+      req.flash('error', "Unable to find users");
+      return res.redirect('back');
+
+    } else if (overlap.length > 0) {
+      req.flash('error', "Another user already has that email.");
       return res.redirect('back');
     }
 
