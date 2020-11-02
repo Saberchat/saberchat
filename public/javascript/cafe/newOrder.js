70a4c028c8910ed69c87468fe92c8f4e1fe11ae7
KabirSamsi <samsikabir@gmail.com>
2020-11-02 13:17:59 -0800

    Improved cafe order form

--- public/javascript/cafe/newOrder.js
+++ public/javascript/cafe/newOrder.js
@@ -1,20 +1,22 @@
 //Collect DOM Elements
 
-const container = document.getElementById('container')
-const dropdown = document.getElementsByTagName('select')
-const fci = document.getElementsByClassName('form-check-input')
-const fcl = document.getElementsByClassName('form-check-label')
-const numOrders = document.getElementsByClassName('num-orders')
-const total = document.getElementById('total-cost')
-const orderConfirm = document.getElementById("order-confirm")
+const container = document.getElementById('container');
+const dropdown = document.getElementsByTagName('select');
+const fci = document.getElementsByClassName('form-check-input');
+const fcl = document.getElementsByClassName('form-check-label');
+const numOrders = document.getElementsByClassName('num-orders');
+const extraInstructionsInput = document.getElementById('descInput');
+const extraInstructions = document.getElementById('extra-instructions');
+const total = document.getElementById('total-cost');
+const orderConfirm = document.getElementById("order-confirm");
 let orderedItem; //Order that will show up in your 'confirm order' page
-let sum = 0
+let sum = 0;
+let instructionsNew;
 let totalNew;
 
-
-//If the main div is clicked, the page will respond
-container.addEventListener('click', () => {
-  sum = 0
+//Changes the order confirmation on the form
+const changeOrderConfirmation = (() => {
+  sum = 0;
 
   while (orderConfirm.firstChild) { //Remove all the items in the 'confirm order' section
     orderConfirm.removeChild(orderConfirm.firstChild);
@@ -51,7 +53,19 @@ container.addEventListener('click', () => {
     }
   }
 
-  //Once this process is finished, create an element to render the total cost
+  //Once this process is finished, create an element to render the extra instructions and total cost
+
+  instructionsNew = document.createElement('span')
+  instructionsNew.style = 'color: blue;';
+  instructionsNew.className = "list-group-item list-group-item-action form-check"
+
+  if (extraInstructionsInput.value == '') {
+    instructionsNew.innerHTML = `<strong>Extra Instructions:</strong> None`;
+
+  } else {
+    instructionsNew.innerHTML = `<strong>Extra Instructions:</strong> ${extraInstructionsInput.value}`;
+  }
+
   totalNew = document.createElement('strong')
   totalNew.style = 'color: green;'
   totalNew.className = "list-group-item list-group-item-action form-check"
@@ -69,67 +83,7 @@ container.addEventListener('click', () => {
   }
 
   //Add the total to the div
+  orderConfirm.appendChild(instructionsNew)
   orderConfirm.appendChild(totalNew)
 
 })
-
-
-for (let dd of dropdown) { //Every quantity dropdown menu
-  dd.onchange = () => { //Be ready for the item amount to change, from the dropdown
-    sum = 0
-
-    //Here, we do the exact same process we did earlier
-    while (orderConfirm.firstChild) {
-      orderConfirm.removeChild(orderConfirm.firstChild);
-    }
-
-    for (let i of fci) {
-      for (let l of fcl) {
-        if (l.htmlFor == i.id) {
-          if (i.checked) {
-
-            for (let no of numOrders) {
-              if (no.id.split('_')[1] == i.id) {
-                sum += parseInt(no.value) * parseFloat(l.innerText.split('$')[1])
-
-                orderedItem = document.createElement('strong')
-                orderedItem.className = "list-group-item list-group-item-action form-check"
-
-
-                if (! (parseInt(no.value) * parseFloat(l.innerText.split('$')[1])).toString().includes('.') ) {
-                  orderedItem.innerText = `${no.name} (${no.value} orders) - \$${parseInt(no.value) * parseFloat(l.innerText.split('$')[1])}.00`
-
-                } else if ((parseInt(no.value) * parseFloat(l.innerText.split('$')[1])).toString().split('.')[1].length == 1){
-                  orderedItem.innerText = `${no.name} (${no.value} orders) - \$${parseInt(no.value) * parseFloat(l.innerText.split('$')[1])}0`
-
-                } else {
-                  orderedItem.innerText = `${no.name} (${no.value} orders) - \$${parseInt(no.value) * parseFloat(l.innerText.split('$')[1])}`
-                }
-
-                orderConfirm.appendChild(orderedItem)
-              }
-            }
-          }
-        }
-      }
-    }
-
-    totalNew = document.createElement('strong')
-    totalNew.style = 'color: green;'
-    totalNew.className = "list-group-item list-group-item-action form-check"
-
-    if (!sum.toString().includes('.')) {
-      totalNew.innerText = `Total: $${sum}.00`
-
-    } else if (sum.toString().split('.')[1].length == 1){
-      totalNew.innerText = `Total: $${sum}0`
-
-    } else {
-      totalNew.innerText = `Total: $${sum}`
-    }
-
-
-    orderConfirm.appendChild(totalNew)
-
-  }
-}
