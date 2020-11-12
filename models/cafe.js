fc29afda5d89d74b35decea94c086b620ff16a79
KabirSamsi <samsikabir@gmail.com>
2020-11-12 11:33:30 -0800

    Some minor changes

--- models/cafe.js
+++ models/cafe.js
@@ -5,7 +5,11 @@ const mongoose = require("mongoose");
 var cafeSchema = new mongoose.Schema({
   open: {type: Boolean, default: false},
   revenue: {type: Number, default: 0},
-  expenditures: {type: Number, default: 0}
+  expenditures: {type: Number, default: 0},
+  types: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'ItemType'
+  }]
 })
 
 module.exports = mongoose.model("Cafe", cafeSchema);
