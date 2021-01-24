a3bae7d6b36372b7933d9905526e0145d47984a3
Kabir Samsi <samsikabir@gmail.com>
2021-01-23 22:42:46 -0800

    Removed .env email variables temporarily

--- transport.js
+++ transport.js
@@ -1,6 +1,6 @@
-if(process.env.NODE_ENV !== "production") {
-	require('dotenv').config();
-}
+// if(process.env.NODE_ENV !== "production") {
+// 	require('dotenv').config();
+// }
 
 const nodemailer = require("nodemailer");
 
@@ -8,13 +8,16 @@ const transport = ((recipient, subject, html) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD
+			user: "noreply.saberchat@gmail.com",
+			pass: "Tgy8erwIYtxRZrJHvKwkWbrkbUhv1Zr9"
+      // user: process.env.EMAIL_ADDRESS,
+      // pass: process.env.EMAIL_PASSWORD
     }
   });
 
   if (process.env.SENDING_EMAILS == "true" && recipient.receiving_emails) {
-    let email = {from: process.env.EMAIL_ADDRESS, to: recipient.email, subject, html};
+		let email = {from: "noreply.saberchat@gmail.com", to: recipient.email, subject, html};
+    // let email = {from: process.env.EMAIL_ADDRESS, to: recipient.email, subject, html};
 
     transporter.sendMail(email, (err, info) => {
       if (err) {
@@ -31,12 +34,15 @@ const transport_mandatory = ((recipient, subject, html) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_ADDRESS,
-      pass: process.env.EMAIL_PASSWORD
+			user: "noreply.saberchat@gmail.com",
+			pass: "Tgy8erwIYtxRZrJHvKwkWbrkbUhv1Zr9"
+      // user: process.env.EMAIL_ADDRESS,
+      // pass: process.env.EMAIL_PASSWORD
     }
   });
 
-  let email = {from: process.env.EMAIL_ADDRESS, to: recipient.email, subject, html};
+  // let email = {from: process.env.EMAIL_ADDRESS, to: recipient.email, subject, html};
+	let email = {from: "noreply.saberchat@gmail.com", to: recipient.email, subject, html};
 
   transporter.sendMail(email, (err, info) => {
     if (err) {
