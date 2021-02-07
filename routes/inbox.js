082dacade41037bc7a6d0639f536bcba0af241ce
Alexander Zhou <alexzhou3999@gmail.com>
2021-02-07 13:55:24 -0800

    moved inbox routes to controller

--- routes/inbox.js
+++ routes/inbox.js
@@ -13,222 +13,24 @@ const User = require('../models/user');
 const Message = require('../models/message');
 const AccessReq = require('../models/accessRequest');
 const Room = require('../models/room');
-const user = require('../models/user');
 
-//Route to display user inbox
-router.get('/', middleware.isLoggedIn, wrapAsync(async (req, res) => {
-	await req.user.populate(
-		{
-			path: 'inbox',
-			populate: {
-				path: 'sender',
-				select: ['username', 'imageUrl']
-			}
-		}).execPopulate();
+// controller
+const inbox = require('../controllers/inbox');
 
-	await req.user.populate(
-		{
-			path: 'requests',
-			populate: [
-				{ path: 'requester', select: ['username', 'imageUrl']},
-				{ path: 'room', select: 'name'}
-			]
-		}).execPopulate();
+// Display user inbox
+router.get('/', middleware.isLoggedIn, wrapAsync(inbox.index));
 
-	let activeRequests = [];
-	for (let request of req.user.requests) {
-		if (request.status == "pending") {
-			activeRequests.push(request);
-		}
-	}
+// Message show route
+router.get('/:id', middleware.isLoggedIn, wrapAsync(inbox.show));
 
-	res.render('inbox/index', {inbox: req.user.inbox.reverse(), requests: req.user.requests.reverse(), activeRequests});
-}));
+// New messsage form
+router.get('/messages/new', middleware.isLoggedIn, wrapAsync(inbox.newMsgForm));
 
+// Display sent messages
+router.get('/sent', middleware.isLoggedIn, wrapAsync(inbox.sent));
 
-//Access sendNotification file
-router.get('/messages/new', middleware.isLoggedIn, wrapAsync(async (req, res) => {
-	const users = await User.find({authenticated: true});
-	if(!users) { req.flash('error', 'Unable to access Database.'); return res.redirect('back'); }
-	res.render('inbox/new', {users: users});
-}));
-
-//Route to send notification to a group of people
-router.post('/messages', middleware.isLoggedIn, validateMsg, (req, res) => {
-	(async () => {
-		let message = {
-			subject: filter.clean(req.body.subject),
-			text: filter.clean(req.body.message)
-		};
-
-		if(req.body.images) {
-			message.images = req.body.images;
-		}
-
-		message.sender = req.user._id;
-		message.noReply = (req.body.noreply == "true");
-
-		let recipients = [];
-
-		if(req.body.recipients) {
-			recipients = JSON.parse(req.body.recipients);
-		}
-
-		if(req.body.all == 'true') {
-			message.toEveryone = true;
-		} else if(!recipients || !recipients.length > 0) {
-			req.flash('error', 'Please select recipients');
-			return res.redirect('back');
-		} else if(recipients.includes(req.user._id)) {
-			req.flash('error', 'You cannot send messages to yourself');
-			return res.redirect('back');
-		} else if(req.body.anonymous == 'true') {
-			const faculty = await User.find({authenticated: true, status: 'faculty', _id: { $in: recipients } });
-
-			if(!faculty) {req.flash('error', 'An error occured'); return res.redirect('back');}
-			if(!faculty.length > 0) {req.flash('error', 'You can only select faculty'); return res.redirect('back');}
-
-			recipients = [];
-			faculty.forEach(user => {
-				recipients.push(user._id);
-			});
-
-			message.anonymous = true;
-			delete message.sender;
-		}
-
-		const statuses = ['7th', '8th', '9th', '10th', '11th', '12th', 'faculty', 'parent', 'alumnus', 'guest'];
-
-		if(req.body.anonymous != 'true' && !message.toEveryone) {
-			let selStatuses = [];
-			for (let i = 0; i < statuses.length; i++) {
-				const status = statuses[i];
-				if(recipients.includes(status)) {
-					selStatuses.push(status);
-					const index = recipients.indexOf(status);
-					recipients.splice(index, 1);
-				}
-			}
-
-			if(selStatuses.length > 0) {
-				const selUsers = await User.find({authenticated: true, status:{$in: selStatuses}});
-
-				if(!selUsers) {
-					req.flash('error', 'Error connecting to database'); return res.redirect('back');
-				}
-
-				for (let i = 0; i < selUsers.length; i++) {
-					const user = selUsers[i];
-					if(!recipients.includes(user._id) && !user._id.equals(req.user._id)) {
-						recipients.push(user._id);
-					}
-				}
-			}
-		}
-		message.recipients = recipients;
-
-		const newMessage = await Message.create(message);
-		if(!newMessage) {
-			req.flash('error', 'Message could not be created'); return res.redirect('back');
-		}
-
-		newMessage.date = dateFormat(newMessage.created_at, "h:MM TT | mmm d");
-		await newMessage.save();
-
-		if(message.toEveryone) {
-			await User.updateMany(
-				{ _id: { $ne: req.user._id } },
-				{
-					$push: { inbox: newMessage },
-					$inc: { msgCount: 1 }
-				});
-
-		} else {
-			await User.updateMany(
-				{ _id: { $in: recipients } },
-				{
-					$push: { inbox: newMessage },
-					$inc: { msgCount: 1 }
-				});
-		}
-
-		let inboxEmail;
-
-		const recipientList = await User.find({authenticated: true, _id: { $in: recipients}});
-
-    let imageString = "";
-
-    for (let image of newMessage.images) {
-      imageString += `<img src="${image}">`;
-    }
-
-    let recipientArr = [];
-
-		for (let r of recipientList) {
-      recipientArr = [];
-
-      for (let rec of recipientList) {
-        if (rec._id.equals(r._id)) {
-          recipientArr.push('me');
-
-        } else {
-          recipientArr.push(rec.username);
-        }
-      }
-
-      if (message.toEveryone) {
-        transport(r, `New Inbox Notification - ${newMessage.subject}`, `<p>Hello ${r.firstName},</p><p>You have a new Saberchat inbox notification from <strong>${req.user.username}</strong>!</p><p><strong>To</strong>: Everyone</p><p>${newMessage.text}</p><p>You can access the full message at https://alsion-saberchat.herokuapp.com</p> ${imageString}`);
-
-      } else if (message.anonymous) {
-        transport(r, `New Inbox Notification - ${newMessage.subject}`, `<p>Hello ${r.firstName},</p><p>You have a new Saberchat anonymous notification!</p><p><strong>To</strong>: ${recipientArr.join(', ')}</p><p>${newMessage.text}</p><p>You can access the full message at https://alsion-saberchat.herokuapp.com</p> ${imageString}`);
-
-      } else {
-        transport(r, `New Inbox Notification - ${newMessage.subject}`, `<p>Hello ${r.firstName},</p><p>You have a new Saberchat inbox notification from <strong>${req.user.username}</strong>!</p><p><strong>To</strong>: ${recipientArr.join(', ')}</p><p>${newMessage.text}</p><p>You can access the full message at https://alsion-saberchat.herokuapp.com</p> ${imageString}`);
-      }
-
-		}
-
-		req.flash('success', 'Message sent');
-		res.redirect(`/inbox/${newMessage._id}`);
-
-	})().catch(err => {
-		req.flash('error', 'An error occured');
-		res.redirect('back');
-	});
-});
-
-//Accesses every notification that you have sent
-router.get('/sent', middleware.isLoggedIn, (req, res) => {
-
-  Message.find({}, (err, messages) => {
-    if (err || !messages) {
-      req.flash('error', 'Unable to access database');
-      res.redirect('/inbox');
-
-    } else {
-      let sent_inbox = []; //Inbox of all messages user has sent
-
-      for (let message of messages) {
-
-        //If this user sent the message
-        if (message.sender.equals(req.user._id))  {
-          sent_inbox.push(message)
-
-        } else {
-          //Check to see if this user has replied to any of the messages
-          for (let reply of message.replies.reverse()) { //Get most  recent reply by reversing replies
-            if (reply.sender.equals(req.user._id)) {
-              sent_inbox.push(message);
-              break;
-            }
-          }
-        }
-      }
-
-      res.render('inbox/index_sent', {inbox: sent_inbox.reverse()});
-    }
-  });
-});
+// Create messsage
+router.post('/messages', middleware.isLoggedIn, validateMsg, wrapAsync(inbox.createMsg));
 
 //User can reply to notifications sent to them
 router.put('/reply', middleware.isLoggedIn, (req, res) => {
@@ -313,110 +115,18 @@ router.put('/reply', middleware.isLoggedIn, (req, res) => {
   });
 });
 
-// Message show route
-router.get('/:id', middleware.isLoggedIn, (req, res) => {
-	( async () => {
-		const message = await Message.findById(req.params.id);
-		if(!message) {
-      req.flash('error','Cannot find message'); return res.redirect('back');
-    }
+// Mark all messages as read
+router.put('/mark-all', middleware.isLoggedIn, wrapAsync(inbox.markReadAll));
 
-		if(!message.toEveryone && !message.recipients.includes(req.user._id) && !message.sender.equals(req.user._id)) {
-			req.flash('error', 'You do not have permission to view this message');
-			return res.redirect('back');
-		}
+// Mark selected messages as read
+router.put('/mark-selected', middleware.isLoggedIn, wrapAsync(inbox.markReadSelected));
 
-		if((!message.read.includes(req.user._id)) && (message.recipients.includes(req.user._id))) {
-			req.user.msgCount -= 1;
-			message.read.push(req.user._id);
-			await req.user.save();
-			await message.save();
-		}
+// Clear entire inbox
+router.delete('/clear', middleware.isLoggedIn, inbox.clear);
 
-		await message.populate({path: 'sender', select: 'username'}).populate({path:'recipients', select: 'username'}).populate({path: 'read', select: 'username'}).populate({path: 'replies.sender'}).execPopulate();
+// Delete messages
+router.delete('/delete', middleware.isLoggedIn, wrapAsync(inbox.delete));
 
-		const convertedText = convertToLink(message.text);
-		res.render('inbox/show', {message: message, convertedText});
-
-  })().catch(err => {
-		req.flash('error','There was an error');
-		res.redirect('back');
-	});
-});
-
-
-//Clear entire inbox
-router.delete('/clear', middleware.isLoggedIn, (req, res) => {
-	req.user.inbox = [];
-	req.user.msgCount = 0;
-	req.user.save();
-	req.flash('success', 'Inbox cleared!');
-	res.redirect('/inbox');
-});
-
-//Delete messages
-router.delete('/delete', middleware.isLoggedIn, (req, res) => {
-	( async ()=> {
-		let ids = [];
-		for(const id in req.body) {
-			ids.push(id);
-		}
-
-		const messages = await Message.find({_id: {$in: ids}});
-		if(!messages) {req.flash('error', 'Could not find messages'); return res.redirect('back');}
-
-		let nUnread = 0;
-		messages.forEach( message => {
-			const i = req.user.inbox.indexOf(message._id);
-			if(i > -1) {
-				req.user.inbox.splice(i, 1);
-				if(!message.read.includes(req.user._id)) {
-					nUnread ++;
-				}
-			}
-		});
-		req.user.msgCount -= nUnread;
-		await req.user.save();
-
-		res.redirect('back');
-	})().catch(err => {
-		req.flash('error', 'An error occured');
-		res.redirect('back');
-	});
-});
-
-router.put('/mark-all', middleware.isLoggedIn, (req, res) => {
-	Message.updateMany({_id: {$in: req.user.inbox}, read: {$ne: req.user._id}}, {$push: {read: req.user._id}}, (err, result) => {
-		if(err) {
-			req.flash('error','Could not mark as read');
-			res.redirect('back');
-		} else {
-			req.user.msgCount -= result.nModified;
-			req.user.save();
-			req.flash('success', 'Marked all as read');
-			res.redirect('back');
-		}
-	});
-});
-
-router.put('/mark-selected', middleware.isLoggedIn, (req, res) => {
-	let ids = [];
-	for(const id in req.body) {
-		ids.push(id);
-	}
-	Message.updateMany({_id: {$in: ids}, read: {$ne: req.user._id}}, {$push: {read: req.user._id}}, (err, result) => {
-		if(err) {
-			req.flash('error','Could not mark as read');
-			res.redirect('back');
-
-		} else {
-			req.user.msgCount -= result.nModified;
-			req.user.save();
-			req.flash('success', 'Marked as read');
-			res.redirect('back');
-		}
-	});
-});
 
 // ========================================
 // access request routes
