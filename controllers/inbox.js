0782a60b31d72a646c19054a5b52c93fe270a202
Alexander Zhou <alexzhou3999@gmail.com>
2021-02-10 15:59:58 -0800

    finished setting up inbox controllers

--- controllers/inbox.js
+++ controllers/inbox.js
@@ -1,13 +1,14 @@
 const dateFormat = require('dateformat');
 const Filter = require('bad-words');
 const filter = new Filter();
-const {transport, transport_mandatory, sendGridEmail} = require("../utils/transport");
+const { sendGridEmail } = require("../utils/transport");
 const convertToLink = require("../utils/convert-to-link");
 
 const User = require('../models/user');
 const Message = require('../models/message');
 const AccessReq = require('../models/accessRequest');
 const Room = require('../models/room');
+const { request } = require('express');
 
 // Inbox GET index page
 module.exports.index = async function(req, res) {
@@ -29,18 +30,13 @@ module.exports.index = async function(req, res) {
 		}
         ).execPopulate();
 
-	let activeRequests = [];
-	for (let request of req.user.requests) {
-		if (request.status == "pending") {
-			activeRequests.push(request);
-		}
-	}
+    const activeReqCount = req.user.requests.filter((req)=>req.status === "pending").length;
 
-	res.render('inbox/index', {inbox: req.user.inbox.reverse(), requests: req.user.requests.reverse(), activeRequests});
+	res.render('inbox/index', {inbox: req.user.inbox.reverse(), requests: req.user.requests.reverse(), reqCount: activeReqCount});
 };
 
 // Inbox GET show message
-module.exports.show = async function(req, res) {
+module.exports.showMsg = async function(req, res) {
     const message = await Message.findById(req.params.id);
 	if(!message) {req.flash('error','Cannot find message'); return res.redirect('back');}
 
@@ -292,7 +288,12 @@ module.exports.reply = async function(req, res) {
         return res.json({error: 'Cannot reply to this message'})
     }
 
-    let reply = {sender: req.user, text: req.body.text, images: req.body.images, date: dateFormat(new Date(), "h:MM TT | mmm d")};
+    let reply = {
+        sender: req.user, 
+        text: req.body.text, 
+        images: req.body.images, 
+        date: dateFormat(new Date(), "h:MM TT | mmm d")
+    };
     message.replies.push(reply); //Add reply to message thread
 
     //Create string to track reply's images
@@ -309,93 +310,55 @@ module.exports.reply = async function(req, res) {
     //Iterates through the recipients and sees if the sender is part of them. If not, then no reply has been sent yet, but since the sender has sent the message, they have 'read' it. Hence, they are added to the readRecipients array.
 
     let senderIncluded = false; //Checks whether the sender is part of the thread
-    for (let recipient of message.recipients) {
+    for (const recipient of message.recipients) {
         if (recipient._id.equals(message.sender._id)) {
             senderIncluded = true;
             break;
         }
     }
-
-
-
-    Message.findById(req.body.message).populate('recipients').populate('sender').exec((err, message) => {
-        if (err || !message) {
-          res.json({error: "Error accessing message"});
-
-        } else if (message.anonymous || message.noReply) {
-          res.json({error: "Cannot reply to this message"});
-
-        } else {
-          let reply = {sender: req.user, text: req.body.text, images: req.body.images, date: dateFormat(new Date(), "h:MM TT | mmm d")};
-          message.replies.push(reply); //Add reply to message thread
-
-          let replyEmail;
-
-          //Create string to track reply's images
-          let imageString = "";
-
-          if (reply.images) {
-            for (let image of reply.images) {
-              imageString += `<img src="${image}">`;
-            }
-          }
-
-          let readRecipients = message.read; //Users who have read the original message will need to have their msgCount incremented again
-
-          //Iterates through the recipients and sees if the sender is part of them. If not, then no reply has been sent yet, but since the sender has sent the message, they have 'read' it. Hence, they are added to the readRecipients array.
-
-          senderIncluded = false; //Checks whether the sender is part of the thread
-          for (let recipient of message.recipients) {
-            if (recipient._id.equals(message.sender._id)) {
-              senderIncluded = true;
-              break;
-            }
-          }
-
-          if (!senderIncluded) {
-            readRecipients.push(message.sender);
-          }
-
-          for (let i = message.recipients.length-1; i >= 0; i--) { //If the original sender is already part of the recipients, remove them just in case
-            if (message.recipients[i]._id.equals(message.sender._id)) {
-              message.recipients.splice(i, 1);
-            }
-          }
-
-          message.recipients.push(message.sender); //Add original sender to recipient list (code above ensures that they are not added multiple times)
-          message.read = [req.user]; //Since the current user replied to this message, they've seen the completely updated message. Nobody else has
-          message.save();
-
-          for (let recipient of message.recipients) { //Remove original message and add it back so that it appears 'new'
-
-            //Remove message from recipient's inbox
-            for (let i = recipient.inbox.length - 1; i >= 0; i --) {
-              if (recipient.inbox[i].equals(message._id)) {
-                recipient.inbox.splice(i, 1);
-              }
-            }
-
-            //Add it to the front of the recipient's inbox
-            recipient.inbox.push(message._id);
-
-            //If the recipient has already read this message and it is not the person sending the reply (or the recipient is the original message sender), increment their message count again
-            if ((readRecipients.includes(recipient._id)) && (!(recipient._id.equals(req.user._id)))) {
-              recipient.msgCount += 1;
-            }
-
-            recipient.save();
-
-            //Send email notifying about the reply to everyone except person who posted the reply
-            if (!(recipient._id.equals(req.user._id))) {
-              transport(recipient, `New Reply On ${message.subject}`, `<p>Hello ${recipient.firstName},</p><p><strong>${req.user.username}</strong> replied to <strong>${message.subject}</strong>.<p>${reply.text}</p><p>You can access the full message at https://alsion-saberchat.herokuapp.com</p> ${imageString}`);
-            }
-          }
-
-          res.json({ //Send JSON response to front-end
-            success: `Replied to ${message._id}`,
-            message: message
-          });
+    if(!senderIncluded) {
+        readRecipients.push(message.sender);
+    }
+    for (let i = message.recipients.length-1; i >= 0; i--) { //If the original sender is already part of the recipients, remove them just in case
+        if (message.recipients[i]._id.equals(message.sender._id)) {
+          message.recipients.splice(i, 1);
         }
+    }
+    
+    message.recipients.push(message.sender); //Add original sender to recipient list (code above ensures that they are not added multiple times)
+    message.read = [req.user]; //Since the current user replied to this message, they've seen the completely updated message. Nobody else has
+    await message.save();
+
+    for (const recipient of message.recipients) { //Remove original message and add it back so that it appears 'new'
+
+        //Remove message from recipient's inbox
+        for (let i = recipient.inbox.length - 1; i >= 0; i --) {
+            if (recipient.inbox[i].equals(message._id)) {
+            recipient.inbox.splice(i, 1);
+            }
+        }
+
+        //Add it to the front of the recipient's inbox
+        recipient.inbox.push(message._id);
+
+        //If the recipient has already read this message and it is not the person sending the reply (or the recipient is the original message sender), increment their message count again
+        if (readRecipients.includes(recipient._id) && !(recipient._id.equals(req.user._id))) {
+            recipient.msgCount += 1;
+        }
+
+        recipient.save();
+
+        //Send email notifying about the reply to everyone except person who posted the reply
+        if (!(recipient._id.equals(req.user._id)) && recipient.receiving_emails) {
+            const email_text = `<p>Hello ${recipient.firstName},</p><p><strong>${req.user.username}</strong> replied to <strong>${message.subject}</strong>.<p>${reply.text}</p><p>You can access the full message at https://alsion-saberchat.herokuapp.com</p> ${imageString}`;
+
+            await sendGridEmail(recipient.email, `New Reply On ${message.subject}`, email_text);
+        }
+    }
+
+    res.json({ //Send JSON response to front-end
+        success: `Replied to ${message._id}`,
+        message: message
     });
 };
 
@@ -421,4 +384,82 @@ module.exports.delete = async function(req, res) {
     await req.user.save();
 
     res.redirect('back');
+};
+
+// ==========================================
+// Access Request Routes
+
+module.exports.showReq = async function(req, res) {
+    const foundReq = await AccessReq.findById(req.params.id)
+    .populate({path: 'requester', select: 'username'})
+    .populate({path: 'room', select: ['creator', 'name']}).exec();
+    if(!foundReq) {req.flash('error', 'Unable to access Database.'); return res.redirect('back');}
+
+    res.render('inbox/requests/show', {request: foundReq});
+};
+
+module.exports.acceptReq = async function(req, res) {
+    const Req = await AccessReq.findById(req.params.id)
+    .populate({path: 'room', select: ['creator']}).populate('requester').exec();
+
+    if(!Req) {
+        req.flash("error", "Unable to access database");
+        return res.redirect('back');
+
+    } else if(!Req.room.creator.id.equals(req.user._id)) {
+        req.flash("error", "You do not have permission to do that");
+        return res.redirect('back');
+
+    } else if(Req.status != 'pending') {
+        req.flash('error', 'Request already handled');
+        return res.redirect('back');
+
+    }
+
+    const foundRoom = await Room.findById(Req.room._id);
+    if(!foundRoom) { req.flash("error", "Unable to access database");return res.redirect('back'); }
+
+    foundRoom.members.push(Req.requester);
+    Req.status = 'accepted';
+    await foundRoom.save();
+    await Req.save();
+
+    if(Req.requester.receiving_emails) {
+        const email_text = `<p>Hello ${Req.requester.firstName},</p><p>Your request to join chat room <strong>${foundRoom.name}</strong> has been accepted!<p><p>You can access the room at https://alsion-saberchat.herokuapp.com</p>`;
+
+        await sendGridEmail(Req.requester.email, `Room Request Accepted - ${foundRoom.name}`, email_text);
+    }
+    
+    req.flash('success', 'Request accepted');
+    res.redirect('/inbox');
+};
+
+module.exports.rejectReq = async function(req, res) {
+    const Req = await AccessReq.findById(req.params.id)
+    .populate('room').populate('requester').exec();
+
+    if(!Req) {
+        req.flash("error", "Unable to access database");
+        return res.redirect('back');
+
+    } else if(!Req.room.creator.id.equals(req.user._id)) {
+        req.flash("error", "You do not have permission to do that");
+        return res.redirect('back');
+
+    } else if(Req.status != 'pending') {
+        req.flash('error', 'Request already handled');
+        return res.redirect('back');
+
+    }
+    Req.status = 'rejected';
+    await Req.save();
+
+    if(Req.requester.receiving_emails) {
+        const email_text = `<p>Hello ${Req.requester.firstName},</p><p>Your request to join chat room <strong>${Req.room.name}</strong> has been rejected. Contact the room creator, <strong>${Req.room.creator.username}</strong>, if you think that there has been a mistake.</p>`;
+
+        await sendGridEmail(Req.requester.email, `Room Request Rejected - ${Req.room.name}`, email_text);
+    }
+
+    req.flash('success', 'Request rejected');
+    res.redirect('/inbox');
 };
--- routes/inbox.js
+++ routes/inbox.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const middleware = require('../middleware');
 const router = express.Router(); //start express router
-const dateFormat = require('dateformat');
-const Filter = require('bad-words');
-const filter = new Filter();
-const {transport, transport_mandatory} = require("../utils/transport");
-const convertToLink = require("../utils/convert-to-link");
 const { validateMsg } = require('../middleware/validation');
 const wrapAsync = require('../utils/wrapAsync');
-
-const User = require('../models/user');
-const Message = require('../models/message');
-const AccessReq = require('../models/accessRequest');
-const Room = require('../models/room');
 
 // controller
 const inbox = require('../controllers/inbox');
@@ -21,7 +11,7 @@ const inbox = require('../controllers/inbox');
 router.get('/', middleware.isLoggedIn, wrapAsync(inbox.index));
 
 // Message show route
-router.get('/:id', middleware.isLoggedIn, wrapAsync(inbox.show));
+router.get('/:id', middleware.isLoggedIn, wrapAsync(inbox.showMsg));
 
 // New messsage form
 router.get('/messages/new', middleware.isLoggedIn, wrapAsync(inbox.newMsgForm));
@@ -33,8 +23,7 @@ router.get('/sent', middleware.isLoggedIn, wrapAsync(inbox.sent));
 router.post('/messages', middleware.isLoggedIn, validateMsg, wrapAsync(inbox.createMsg));
 
 //User can reply to notifications sent to them
-router.put('/reply', middleware.isLoggedIn, (req, res) => {
-});
+router.put('/reply', middleware.isLoggedIn, wrapAsync(inbox.reply));
 
 // Mark all messages as read
 router.put('/mark-all', middleware.isLoggedIn, wrapAsync(inbox.markReadAll));
@@ -54,88 +43,12 @@ router.delete('/delete', middleware.isLoggedIn, wrapAsync(inbox.delete));
 // ========================================
 
 // displays single access request
-router.get('/requests/:id', middleware.isLoggedIn, (req, res) => {
-	AccessReq.findById(req.params.id)
-	.populate({path: 'requester', select: 'username'})
-	.populate({path: 'room', select: ['creator', 'name']}).exec((err, foundReq) => {
-		if(err || !foundReq) {
-			req.flash("error", "Unable to access database");
-			res.redirect('back');
-		} else {
-			res.render('inbox/requests/show', {request: foundReq});
-		}
-	});
-});
+router.get('/requests/:id', middleware.isLoggedIn, wrapAsync(inbox.showReq));
 
 // route to accept request
-router.post('/requests/:id/accept', middleware.isLoggedIn, (req, res) => {
-	(async () =>  {
-		const Req = await AccessReq.findById(req.params.id)
-		.populate({path: 'room', select: ['creator']}).populate('requester');
-
-		if(!Req) {
-			req.flash("error", "Unable to access database");
-			return res.redirect('back');
-
-		} else if(!Req.room.creator.id.equals(req.user._id)) {
-			req.flash("error", "You do not have permission to do that");
-			return res.redirect('back');
-
-		} else if(Req.status != 'pending') {
-			req.flash('error', 'Request already handled');
-			return res.redirect('back');
-
-		} else {
-			const foundRoom = await Room.findById(Req.room._id);
-			if(!foundRoom) { req.flash("error", "Unable to access database");return res.redirect('back'); }
-			foundRoom.members.push(Req.requester);
-			Req.status = 'accepted';
-			await foundRoom.save();
-			await Req.save();
-
-      		transport(Req.requester, `Room Request Accepted - ${foundRoom.name}`, `<p>Hello ${Req.requester.firstName},</p><p>Your request to join chat room <strong>${foundRoom.name}</strong> has been accepted!<p><p>You can access the room at https://alsion-saberchat.herokuapp.com</p>`);
-			req.flash('success', 'Request accepted');
-			return res.redirect('/inbox');
-		}
-
-	})().catch(err => {
-		req.flash("error", "Unable to access database");
-		res.redirect('back');
-	});
-});
+router.post('/requests/:id/accept', middleware.isLoggedIn, wrapAsync(inbox.acceptReq));
 
 // route to reject request
-router.post('/requests/:id/reject', middleware.isLoggedIn, (req, res) => {
-	( async () => {
-		const Req = await AccessReq.findById(req.params.id)
-		.populate('room').populate('requester')
-
-		if(!Req) {
-			req.flash("error", "Unable to access database");
-			return res.redirect('back');
-
-		} else if(!Req.room.creator.id.equals(req.user._id)) {
-			req.flash("error", "You do not have permission to do that");
-			return res.redirect('back');
-
-		} else if(Req.status != 'pending') {
-			req.flash('error', 'Request already handled');
-			return res.redirect('back');
-
-		} else {
-
-			Req.status = 'rejected';
-			await Req.save();
-
-      		transport(Req.requester, `Room Request Rejected - ${Req.room.name}`, `<p>Hello ${Req.requester.firstName},</p><p>Your request to join chat room <strong>${Req.room.name}</strong> has been rejected. Contact the room creator, <strong>${Req.room.creator.username}</strong>, if  you think there has been a mistake.</p>`);
-			req.flash('success', 'Request rejected');
-			return res.redirect('/inbox');
-		}
-
-	})().catch(err => {
-		req.flash("error", "Unable to access database");
-		res.redirect('back');
-	});
-});
+router.post('/requests/:id/reject', middleware.isLoggedIn, wrapAsync(inbox.rejectReq));
 
 module.exports = router;
