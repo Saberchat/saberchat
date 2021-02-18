6b0a6e43c5180faed3730c12fe282b932bde10f6
Kabir Samsi <samsikabir@gmail.com>
2021-02-18 12:15:46 -0800

    Fixed email function

--- controllers/index.js
+++ controllers/index.js
@@ -9,7 +9,6 @@ const User = require('../models/user');
 const Course = require('../models/homework/course');
 const Email = require('../models/admin/email');
 const Announcement = require('../models/announcements/announcement');
-const Item = require('../models/cafe/orderItem');
 
 module.exports.index = function(req, res) {
     return res.render('index');
