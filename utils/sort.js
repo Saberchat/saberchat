7da552843e01ef4e200fe431c8572c0a13ea7445
Kabir Samsi <samsikabir@gmail.com>
2021-10-05 22:24:55 -0700

    Implemented recursive quicksort algorithm. Next steps -> initialize sitewide

---
+++ utils/sort.js
@@ -0,0 +1,37 @@
+const package = {};
+
+package.partition = function(arr, from, to) { //Partition array around pivot and swap misplaced elements on either side
+    let pivot = arr[from]; //Set pivot to any value in array (start works nicely)
+    
+    //Set tracker variables on either side
+    let i = from-1;
+    let j = to+1;
+
+    while (i < j) { //Continue swapping process until i moves past j
+        i++;
+        while (arr[i] < pivot) {i++;} //Find first value larger than pivot on the left
+        j--;
+        while (arr[j] > pivot) {j--;} //Find first value smaller than pivot on the right
+        if (i < j) { //If i and j are both on the correct sides of the pivot, swap elements on either side
+            [arr[i], arr[j]] = [arr[j], arr[i]]; //Swap elements
+        }
+    }
+    return j;
+}
+
+package.quicksort = function(arr, from, to) { //Implement recursive quicksort algorithm on array
+    if (from < to) { //Continue until pivot falls outside array bounds
+        let p = partition(arr, from, to); //Create pivot point by partitioning array into two parts
+
+        //Recursively sort around pivot
+        quicksort(arr, from, p);
+        quicksort(arr, p+1, to);
+    }
+}
+
+let arr3 = [69, 43, 12, 135, 24, 564, 232, 4];
+console.log(arr3);
+quicksort(arr3, 0, arr3.length-1);
+console.log(arr3);
+
+module.exports = package;
