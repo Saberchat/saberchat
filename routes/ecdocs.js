ab042e9660c62dd648a7e08a7f3eae8b75b75163
KabirSamsi <samsikabir@gmail.com>
2022-01-03 17:15:07 +0530

    Document Permissions Hotfix

--- routes/ecdocs.js
+++ routes/ecdocs.js
@@ -5,7 +5,7 @@ const ecdocs = require('../controllers/ecdocs'); //Controller
 const router = express.Router(); //Router
 
 router.get('/', wrapAsync(ecdocs.index)); //EC info
-router.get('/edit', middleware.isLoggedIn, middleware.isAdmin, wrapAsync(ecdocs.edit)); //Edit EC info form
-router.put('/', middleware.isLoggedIn, middleware.isAdmin, wrapAsync(ecdocs.update)); //Update EC info
+router.get('/edit', middleware.isLoggedIn, middleware.isPrincipal, wrapAsync(ecdocs.edit)); //Edit EC info form
+router.put('/', middleware.isLoggedIn, middleware.isPrincipal, wrapAsync(ecdocs.update)); //Update EC info
 
 module.exports = router;
