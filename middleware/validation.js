766d279ecd265955a2b49f74822a0c387c65ef29
Alexander Zhou <alexzhou3999@gmail.com>
2022-01-30 14:55:49 -0800

    middleware commenting

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,3 +1,6 @@
+// Backend validation for schemas
+
+// Import schema blueprints
 const {annSchema} = require('../joiValidation/announcement');
 const {projectSchema} = require('../joiValidation/project');
 const {chatSchema} = require('../joiValidation/chat');
@@ -5,6 +8,7 @@ const {msgSchema} = require('../joiValidation/message');
 const {courseSchema} = require('../joiValidation/course');
 const user = require('../joiValidation/user');
 
+// Validate POST request body data based on schema
 const handleValidation = function(schema, req, res, next) {
     const {error} = schema.validate(req.body);
     if (error) {
@@ -46,22 +50,27 @@ module.exports.validatePasswordReset = function(req, res, next) {
     handleValidation(user.resetPasswordSchema, req, res, next);
 };
 
+// new User creation form
 module.exports.validateNewUser = function(req, res, next) {
     handleValidation(user.newUserSchema, req, res, next);
 };
 
+// Login form
 module.exports.validateUserLogin = function(req, res, next) {
     handleValidation(user.loginUserSchema, req, res, next);
 };
 
+// Update profile form
 module.exports.validateUserUpdate = function(req, res, next) {
     handleValidation(user.updateUserSchema, req, res, next);
 };
 
+// Change email form
 module.exports.validateEmailUpdate = function(req, res, next) {
     handleValidation(user.updateEmailSchema, req, res, next);
 };
 
+// Change password form
 module.exports.validatePasswordUpdate = function(req, res, next) {
     handleValidation(user.updatePasswordSchema, req, res, next);
 };
